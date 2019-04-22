7f8a09eb94f49ed629297b786cbc4a1e45f5b70c
Doug <me@douglasbowen.com>
2019-04-22 18:06:05 -0400

    footer

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -4,7 +4,12 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className="footer">
-        
+        <i class="fas fa-comment-dots"></i>
+        <i class="fas fa-retweet"></i>6
+        <i class="fas fa-heart"></i>4
+        <i class="far fa-envelope"></i>
         </div>
     );
 }
+
+export default Footer;
